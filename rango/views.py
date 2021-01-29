102db1ee587e97ed162ee7fbd46ff3f1647028b2
Liang420436 <2512275l@student.gla.ac.uk>
2021-01-29 21:10:38 +0000

    chapter3

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<a href='/rango/about/'>About</a>")
+
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page!<br/>"
+        "<a href='/rango/'>Index</a>")
