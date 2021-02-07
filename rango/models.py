3a5a50732fd07700ef9ab6b3a4ce36b3252733a8
Liang420436 <2512275l@student.gla.ac.uk>
2021-02-07 13:04:07 +0000

    chapter 5

--- rango/models.py
+++ rango/models.py
@@ -5,9 +5,15 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0, null=True)
+    likes = models.IntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Page(models.Model):
@@ -15,3 +21,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
